8975d3549414a3111261e07ccd3e6897913dad2a
AbdelwahapBakdones <abdaelwahapbak@gmail.com>
2025-02-11 12:09:32 +0300

    Add requirements.txt file

--- addons/addons_repzo/__manifest__.py
+++ addons/addons_repzo/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '1.1',
     'author': 'AbdElwahap',
     'category': 'EndPoint',
-    'summary': 'API Endpoint for reepzo',
+    'summary': 'API Endpoint for Repzo',
     'depends': ['base', 'contacts', 'account', 'sale'],
     'external_dependencies': {
         'python': ['marshmallow>=3.15.0,<3.23.1'],
